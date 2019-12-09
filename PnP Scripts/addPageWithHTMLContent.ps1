db3d3d8efdf011f67514d1b9c87f9db7bd68837a
Cindy Bousiou <sindyboussiou@gmail.com>
2019-12-09 11:02:27 +0200

    Add HTML page script

--- PnP Scripts/addPageWithHTMLContent.ps1
+++ PnP Scripts/addPageWithHTMLContent.ps1
@@ -4,7 +4,7 @@ $UserName = "e82331@nbg.gr"
 $pwd = "123sindy^"
 [SecureString]$SecurePwd = ConvertTo-SecureString $pwd -AsPlainText -Force
 $Credentials = New-Object System.Management.Automation.PSCredential($UserName,$SecurePwd)
-$Url = "https://groupnbg.sharepoint.com/sites/TranformationQA"
+$Url = "https://groupnbg.sharepoint.com/sites/communicationtopic"
 Connect-PnPOnline -Url $Url -Credentials $Credentials
 
 $inputUrl = "C:\Users\KyriakiBousiou\Desktop\html.csv"
@@ -13,7 +13,5 @@ $file = Import-Csv $inputUrl
 foreach($record in $file) {  
     $page = Add-PnPClientSidePage -Name $record.Title -Publish
     Add-PnPClientSideText -Page $record.Title -Text $record.html
-    #$pageUrl = "/sites/TranformationQA/SitePages/"+$record.Title+".aspx"
-    #Set-PnPFileCheckedIn -Url $pageUrl -CheckinType MajorCheckIn
     Set-PnPClientSidePage -Identity $record.Title -Publish
 }
