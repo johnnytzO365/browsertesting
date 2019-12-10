5e0afa103ae6f00632f68313d6eb1fd8edb24590
John Tzanos <johnny.tz@gmail.com>
2019-12-10 16:10:28 +0200

    Script can now read data from Excel file.

--- PnP Scripts/addPageWithHTMLContent.ps1
+++ PnP Scripts/addPageWithHTMLContent.ps1
@@ -1,19 +1,21 @@
-﻿#First run Install-Module -Name ImportExcel
+﻿Import-Module SharePointPnPPowerShellOnline
 
-Import-Module SharePointPnPPowerShellOnline
-
-$UserName = "e82331@nbg.gr"
-$pwd = "123sindy^"
+$UserName = ""
+$pwd = ""
 [SecureString]$SecurePwd = ConvertTo-SecureString $pwd -AsPlainText -Force
 $Credentials = New-Object System.Management.Automation.PSCredential($UserName,$SecurePwd)
-$Url = "https://groupnbg.sharepoint.com/sites/communicationtopic"
+$Url = "https://devnbg.sharepoint.com"
 Connect-PnPOnline -Url $Url -Credentials $Credentials
 
-$inputUrl = "C:\Users\KyriakiBousiou\Desktop\html.xlsx"
-$file = Import-Excel -Path $inputUrl
+$inputPath = "C:\Users\IoannisTzanos\Desktop\NewsMigration\html.xlsx"
+$Excel = New-Object -ComObject Excel.Application
+$workbook = $Excel.Workbooks.Open($inputPath)
+$workSheet = $workbook.Sheets.Item(1)
+$currentRowCount = $workSheet.Rows.CurrentRegion.Rows.Count
 
-foreach($record in $file) {  
-    $page = Add-PnPClientSidePage -Name $record.Title -Publish
-    Add-PnPClientSideText -Page $record.Title -Text $record.html
-    Set-PnPClientSidePage -Identity $record.Title -Publish
+for($i=2;$i -le $currentRowCount;$i++) {  
+    $pageTitle = $workSheet.Cells($i,1).Text
+    $page = Add-PnPClientSidePage -Name $pageTitle -Publish
+    Add-PnPClientSideText -Page $pageTitle -Text $workSheet.Cells($i,2).Text
+    Set-PnPClientSidePage -Identity $pageTitle -Publish
 }
