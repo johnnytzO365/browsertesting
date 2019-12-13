d19a527e87a22cede0f0335913d70bbaafdfa646
Cindy Bousiou <sindyboussiou@gmail.com>
2019-12-13 13:17:39 +0200

    Add Page with HTML script with logging

--- PnP Scripts/addPageWithHTMLContent.ps1
+++ PnP Scripts/addPageWithHTMLContent.ps1
@@ -20,16 +20,86 @@
 
 #-----------------------------------[Initialisations]----------------------------------------------------------
 Import-Module SharePointPnPPowerShellOnline
-$ErrorActionPreference="SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
 
 #----------------------------------[Declarations]--------------------------------------------------------------
-$inputPath = "C:\Users\KyriakiBousiou\Desktop\html.xlsx"
-$LogPath = "C:\Users\KyriakiBousiou\Desktop"
-$LogName = "PnPSciptLogFile.log"
-$LogFile = Join-Path -Path $LogPath -ChildPath $LogName
+$inputPath = "C:\Users\KyriakiBousiou\Desktop\htmls.xlsx"
+$LogPath = "C:\Users\KyriakiBousiou\Desktop\Log.log"
 
 #-------------------------------------[Functions]-------------------------------------------------------------
-
+function Write-Log 
+{ 
+    [CmdletBinding()] 
+    Param 
+    ( 
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipelineByPropertyName=$true)] 
+        [ValidateNotNullOrEmpty()] 
+        [Alias("LogContent")] 
+        [string]$Message, 
+ 
+        [Parameter(Mandatory=$false)] 
+        [Alias('LogPath')] 
+        [string]$Path='C:\Logs\PowerShellLog.log', 
+         
+        [Parameter(Mandatory=$false)] 
+        [ValidateSet("Error","Warn","Info")] 
+        [string]$Level="Info", 
+         
+        [Parameter(Mandatory=$false)] 
+        [switch]$NoClobber 
+    ) 
+ 
+    Begin 
+    { 
+        # Set VerbosePreference to Continue so that verbose messages are displayed. 
+        $VerbosePreference = 'Continue' 
+    } 
+    Process 
+    { 
+         
+        # If the file already exists and NoClobber was specified, do not write to the log. 
+        if ((Test-Path $Path) -AND $NoClobber) { 
+            Write-Error "Log file $Path already exists, and you specified NoClobber. Either delete the file or specify a different name." 
+            Return 
+            } 
+ 
+        # If attempting to write to a log file in a folder/path that doesn't exist create the file including the path. 
+        elseif (!(Test-Path $Path)) { 
+            Write-Verbose "Creating $Path." 
+            $NewLogFile = New-Item $Path -Force -ItemType File 
+            } 
+ 
+        else { 
+            # Nothing to see here yet. 
+            } 
+ 
+        # Format Date for our Log File 
+        $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss" 
+ 
+        # Write message to error, warning, or verbose pipeline and specify $LevelText 
+        switch ($Level) { 
+            'Error' { 
+                Write-Error $Message 
+                $LevelText = 'ERROR:' 
+                } 
+            'Warn' { 
+                Write-Warning $Message 
+                $LevelText = 'WARNING:' 
+                } 
+            'Info' { 
+                Write-Verbose $Message 
+                $LevelText = 'INFO:' 
+                } 
+            } 
+         
+        # Write log entry to $Path 
+        "$FormattedDate $LevelText $Message" | Out-File -FilePath $Path -Append 
+    } 
+    End 
+    { 
+    } 
+}
 #--------------------------------------[Execution]------------------------------------------------------------
 #Connect
 $UserName = "sindy@bousiou.onmicrosoft.com"
@@ -38,40 +108,45 @@ $pwd = "Gld9q_31"
 $Credentials = New-Object System.Management.Automation.PSCredential($UserName,$SecurePwd)
 $Url = "https://bousiou.sharepoint.com/sites/testSite"
 
-Log-Start -LogPath $LogPath -LogName $LogName
-
 try{
     Connect-PnPOnline -Url $Url -Credentials $Credentials
+    Write-Host "Connected"
 }
 catch{
     Write-Host "Connection Problem. Check the Url and your credentials and try again"
-    #Log-Error -LogPath $LogFile -ErrorDesc $_.Exception -ExitGracefully $True
+    Write-Log -Message "Couldn't connect: $_" -Path $LogPath -Level Error
     Exit(1)
 }
 
 #Open input file
-$Excel = New-Object -ComObject Excel.Application
-$workbook = $Excel.Workbooks.Open($inputPath)
+try{
+    $Excel = New-Object -ComObject Excel.Application
+    $workbook = $Excel.Workbooks.Open($inputPath)
+}
+catch{
+    Write-Host "Problem with xlsx file. Check the path and the type of file and try again."
+    Write-Log -Message "Couldn't open input xlsx file: $_" -Path $LogPath -Level Error
+    Exit(2)
+}
 $workSheet = $workbook.Sheets.Item(1)
 $currentRowCount = $workSheet.Rows.CurrentRegion.Rows.Count
 
 #loop to each line
 for($i=2;$i -le $currentRowCount;$i++) {  
     $pageTitle = $workSheet.Cells($i,1).Text
+    $html = $workSheet.Cells($i,2).Text
+    $newhtml = $html.Replace("InternalCom/","sites/testSite/")
 
     try{
         $page = Add-PnPClientSidePage -Name $pageTitle -Publish
-        Add-PnPClientSideText -Page $pageTitle -Text $workSheet.Cells($i,2).Text
+        Add-PnPClientSideText -Page $pageTitle -Text $newhtml
         Set-PnPClientSidePage -Identity $pageTitle -Publish
-        $message = "\n"+$pageTitle+": Completed succesfully\n"
-        #Log-Write -LogPath $LogFile -LineValue $message
+        $message = $pageTitle+": Completed succesfully"
+        Write-Log -Message "$pageTitle : Completed successfully" -Path $LogPath -Level Info
     }
     catch{
         Write-Host "Problem with page:" $pageTitle
-        #Log-Write -LogPath $LogFile -LineValue $pageTitle
-        #Log-Error -LogPath $LogFile -ErrorDesc $_.Exception
+        Write-Log -Message "Problem with $pageTitle : $_" -Path $LogPath -Level Warn
         continue
     }
 }
-
-Log-Finish -LogPath $LogFile
