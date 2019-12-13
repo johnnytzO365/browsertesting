af5faf89e14ee8828b88a0962f9de951c1693fe9
Cindy Bousiou <sindyboussiou@gmail.com>
2019-12-13 16:14:03 +0200

    Write-Log Module

--- PnP Scripts/addPageWithHTMLContent.ps1
+++ PnP Scripts/addPageWithHTMLContent.ps1
@@ -20,6 +20,7 @@
 
 #-----------------------------------[Initialisations]----------------------------------------------------------
 Import-Module SharePointPnPPowerShellOnline
+Import-Module 'C:\Users\KyriakiBousiou\Desktop\PnP Powershell Scripts\WriteLogModule.psm1'
 $ErrorActionPreference = "SilentlyContinue"
 
 #----------------------------------[Declarations]--------------------------------------------------------------
@@ -27,79 +28,7 @@ $inputPath = "C:\Users\KyriakiBousiou\Desktop\htmls.xlsx"
 $LogPath = "C:\Users\KyriakiBousiou\Desktop\Log.log"
 
 #-------------------------------------[Functions]-------------------------------------------------------------
-function Write-Log 
-{ 
-    [CmdletBinding()] 
-    Param 
-    ( 
-        [Parameter(Mandatory=$true, 
-                   ValueFromPipelineByPropertyName=$true)] 
-        [ValidateNotNullOrEmpty()] 
-        [Alias("LogContent")] 
-        [string]$Message, 
- 
-        [Parameter(Mandatory=$false)] 
-        [Alias('LogPath')] 
-        [string]$Path='C:\Logs\PowerShellLog.log', 
-         
-        [Parameter(Mandatory=$false)] 
-        [ValidateSet("Error","Warn","Info")] 
-        [string]$Level="Info", 
-         
-        [Parameter(Mandatory=$false)] 
-        [switch]$NoClobber 
-    ) 
- 
-    Begin 
-    { 
-        # Set VerbosePreference to Continue so that verbose messages are displayed. 
-        $VerbosePreference = 'Continue' 
-    } 
-    Process 
-    { 
-         
-        # If the file already exists and NoClobber was specified, do not write to the log. 
-        if ((Test-Path $Path) -AND $NoClobber) { 
-            Write-Error "Log file $Path already exists, and you specified NoClobber. Either delete the file or specify a different name." 
-            Return 
-            } 
- 
-        # If attempting to write to a log file in a folder/path that doesn't exist create the file including the path. 
-        elseif (!(Test-Path $Path)) { 
-            Write-Verbose "Creating $Path." 
-            $NewLogFile = New-Item $Path -Force -ItemType File 
-            } 
- 
-        else { 
-            # Nothing to see here yet. 
-            } 
- 
-        # Format Date for our Log File 
-        $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss" 
- 
-        # Write message to error, warning, or verbose pipeline and specify $LevelText 
-        switch ($Level) { 
-            'Error' { 
-                Write-Error $Message 
-                $LevelText = 'ERROR:' 
-                } 
-            'Warn' { 
-                Write-Warning $Message 
-                $LevelText = 'WARNING:' 
-                } 
-            'Info' { 
-                Write-Verbose $Message 
-                $LevelText = 'INFO:' 
-                } 
-            } 
-         
-        # Write log entry to $Path 
-        "$FormattedDate $LevelText $Message" | Out-File -FilePath $Path -Append 
-    } 
-    End 
-    { 
-    } 
-}
+
 #--------------------------------------[Execution]------------------------------------------------------------
 #Connect
 $UserName = "sindy@bousiou.onmicrosoft.com"
@@ -132,7 +61,13 @@ $workSheet = $workbook.Sheets.Item(1)
 $currentRowCount = $workSheet.Rows.CurrentRegion.Rows.Count
 
 #loop to each line
-for($i=2;$i -le $currentRowCount;$i++) {  
+if($args[0] -eq $null){
+    $startline =2
+}
+else{
+    $startline=$args[0]
+}
+for($i=$startline;$i -le $currentRowCount;$i++) {  
     $pageTitle = $workSheet.Cells($i,1).Text
     $html = $workSheet.Cells($i,2).Text
     $newhtml = $html.Replace("InternalCom/","sites/testSite/")
