7991a4bf29338a5243cc3eed1f7042154a98f15e
Haris Giannopoulos <50793970+HarisMaster@users.noreply.github.com>
2019-12-16 12:00:53 +0200

    Update Link.cs

--- SeleniumTutorial/Link.cs
+++ SeleniumTutorial/Link.cs
@@ -9,21 +9,50 @@ using OpenQA.Selenium.Chrome;
 using System.Net;
 using System.Threading;
 using System.Configuration;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Support.UI;
+using System.Diagnostics;
+using System.ComponentModel;
+using OpenQA.Selenium.IE;
 
 namespace SeleniumTutorial
 {
     class Link
     {
         IWebDriver driver;
+        WebDriverWait wait;
         [SetUp]
         public void StartBrowser()
         {
-            driver = new ChromeDriver(ConfigurationManager.AppSettings["ChromeDriverPath"]);
+            driver = new InternetExplorerDriver(ConfigurationManager.AppSettings["ChromeDriverPath"]);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(40));
+            driver.Navigate().GoToUrl((ConfigurationManager.AppSettings["ServerName"]));
+            Thread.Sleep(2000);
+
+            driver.FindElement(By.XPath("//*[@id='ctl00_PlaceHolderMain_ClaimsLogonSelector']")).Click();
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='ctl00_PlaceHolderMain_ClaimsLogonSelector']/option[2]")));
+            driver.FindElement(By.XPath("//*[@id='ctl00_PlaceHolderMain_ClaimsLogonSelector']/option[3]")).Click();
+            try
+            {
+                using (Process myProcess = new Process())
+                {
+                    myProcess.StartInfo.FileName = ConfigurationManager.AppSettings["ScriptPath"];
+                    myProcess.Start();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Thread.Sleep(4000);
+
+            
         }
         [Test]
         public void CheckLinksOnGlobalNav()
         {
-            driver.Navigate().GoToUrl((ConfigurationManager.AppSettings["ServerName"]) + "el");
+            
 
             IList<IWebElement> links = driver.FindElements(By.ClassName("expanded"));
             foreach (IWebElement link in links)
@@ -37,17 +66,16 @@ namespace SeleniumTutorial
         [Test]
         public void CheckRetailNode()
         {
-            driver.Navigate().GoToUrl((ConfigurationManager.AppSettings["ServerName"]) + "el");
-            var retail = driver.FindElement(By.ClassName("megamenu"));
+
+            var retail = driver.FindElement(By.XPath("//*[@id='DeltaPlaceHolderMain']/div/div[1]/div/div[1]/div/ul/li[1]/ul"));
             List<IWebElement> links = retail.FindElements(By.CssSelector("a")).ToList();
-            Assert.AreEqual(34, links.Count);//μετράει τα links που είναι μέσα στο local navigation
+            Assert.AreEqual(8, links.Count);//μετράει τα links που είναι μέσα στο local navigation
         }
 
         [Test]
         public void CheckLinksOnRetail()
         {
-            driver.Navigate().GoToUrl((ConfigurationManager.AppSettings["ServerName"]) + "el");
-
+         
             IWebElement parentMenu = driver.FindElement(By.XPath("//*[@id='DeltaPlaceHolderMain']/div/div[1]/div/div[2]/div/ul/li[1]/a"));
             parentMenu.Click();
             IList<IWebElement> links = driver.FindElements(By.ClassName("megamenu")).ToList();
@@ -67,7 +95,7 @@ namespace SeleniumTutorial
         [Test]
         public void CheckLinksOnBuss()
         {
-            driver.Navigate().GoToUrl((ConfigurationManager.AppSettings["ServerName"]) + "el");
+         
 
             IWebElement parentMenu = driver.FindElement(By.XPath("//*[@id='DeltaPlaceHolderMain']/div/div[1]/div/div[2]/div/ul/li[2]/a"));
             parentMenu.Click();
@@ -88,7 +116,7 @@ namespace SeleniumTutorial
         [Test]
         public void CheckLinksOnCorp()
         {
-            driver.Navigate().GoToUrl((ConfigurationManager.AppSettings["ServerName"]) + "el");
+            
 
             IWebElement parentMenu = driver.FindElement(By.XPath("//*[@id='DeltaPlaceHolderMain']/div/div[1]/div/div[2]/div/ul/li[3]/a"));
             parentMenu.Click();
@@ -109,7 +137,6 @@ namespace SeleniumTutorial
         [Test]
         public void CheckALink()
         {
-            driver.Navigate().GoToUrl((ConfigurationManager.AppSettings["ServerName"]) + "el");
             IWebElement parentMenu = driver.FindElement(By.XPath("//*[@id='DeltaPlaceHolderMain']/div/div[1]/div/div[2]/div/ul/li[1]/a"));
             parentMenu.Click();
             IWebElement brokenLink = parentMenu.FindElement(By.XPath("//*[@id='ctl00_PlaceHolderCustomHeader_PlaceHolderCustomHeaderMain_ctl09_DefaultMobilePanel_tabsRepeater_ctl00_mainCategoriesRepeater_ctl06_MenuItemPanel']/ul/li[1]/a"));
