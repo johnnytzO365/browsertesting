a24b70748e3338423cabf901f1150639a031af79
Haris Giannopoulos <50793970+HarisMaster@users.noreply.github.com>
2019-12-16 14:15:47 +0200

    Change

---
+++ CookieChecker/CopyPasteSPPages/ExplorerCookies.cs
@@ -0,0 +1,238 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.IE;
+
+namespace CookieChecker
+{
+    
+    class ExplorerCookies
+    {
+        IWebDriver driver = new InternetExplorerDriver(@"C:\\Users\\e82337\\Downloads\\browsertesting\\browsertesting\\.nuget");
+        WebDriverWait wait;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver.Manage().Window.Maximize();  //to use the desired width of window
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void CookiesBarAcceptAll()
+        {
+            String line;
+            System.IO.StreamReader infile = new System.IO.StreamReader(@"C:\Users\e82337\Desktop\domainUrl.txt");
+
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            Microsoft.Office.Interop.Excel._Worksheet oSheet;
+            oXL = new Microsoft.Office.Interop.Excel.Application();
+            oXL.Visible = true;
+            oWB = (Microsoft.Office.Interop.Excel._Workbook)(oXL.Workbooks.Add(""));
+            oSheet = (Microsoft.Office.Interop.Excel._Worksheet)oWB.ActiveSheet;
+
+            oSheet.Cells[1, 1] = "Domain";
+            oSheet.Cells[1, 2] = "Cookies";
+
+            int counter = 2;
+            while ((line = infile.ReadLine()) != null)
+            {
+                int c2 = 3;
+                driver.Navigate().GoToUrl(line);
+
+                try
+                {
+                    wait.Until(ExpectedConditions.ElementIsVisible(By.Id("cookGRALL")));//publicSite CookiesBar
+                    driver.FindElement(By.Id("cookGRALL")).Click();
+                }
+                catch
+                {
+                    try
+                    {
+                        wait.Until(ExpectedConditions.ElementIsVisible(By.Id("ccc-notify-accept")));//wizz
+                        driver.FindElement(By.Id("ccc-notify-accept")).Click();
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("checkbox_icon")));//developers
+                            driver.FindElement(By.CssSelector("checkbox_icon")).Click();
+                            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("btn electro")));
+                            driver.FindElement(By.CssSelector("btn electro")).Click();
+                        }
+                        catch
+                        {
+                            try
+                            {
+                                wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='cookie - bar']/div/div[2]/div[2]/button")));//act4Greece
+                                driver.FindElement(By.XPath("//*[@id='cookie - bar']/div/div[2]/div[2]/button")).Click();
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+                    }
+
+                }
+                int number = driver.Manage().Cookies.AllCookies.Count;
+                List<Cookie> cooks = driver.Manage().Cookies.AllCookies.ToList();
+                oSheet.Cells[counter, 1] = line;
+                oSheet.Cells[counter, 2] = number;
+                foreach (Cookie cook in cooks)
+                {
+                    oSheet.Cells[counter, c2] = cook.Name;
+                    c2++;
+                }
+                counter++;
+                driver.Manage().Cookies.DeleteAllCookies();
+            }
+
+            infile.Close();
+
+            oXL.Visible = false;
+            oXL.UserControl = false;
+            oWB.SaveAs(@"C:\Users\e82337\Desktop\CookiesAccept.xlsx", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            oWB.Close();
+            oXL.Quit();
+        }
+
+        [Test]
+        public void CookiesBarRejectAll()
+        {
+            String line;
+            System.IO.StreamReader infile = new System.IO.StreamReader(@"C:\Users\e82337\Desktop\domainUrl.txt");
+
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            Microsoft.Office.Interop.Excel._Worksheet oSheet;
+            oXL = new Microsoft.Office.Interop.Excel.Application();
+            oXL.Visible = true;
+            oWB = (Microsoft.Office.Interop.Excel._Workbook)(oXL.Workbooks.Add(""));
+            oSheet = (Microsoft.Office.Interop.Excel._Worksheet)oWB.ActiveSheet;
+
+            oSheet.Cells[1, 1] = "Domain";
+            oSheet.Cells[1, 2] = "Cookies";
+
+            int counter = 2;
+            while ((line = infile.ReadLine()) != null)
+            {
+                int c2 = 3;
+                driver.Navigate().GoToUrl(line);
+
+                try
+                {
+                    wait.Until(ExpectedConditions.ElementIsVisible(By.Id("cookGRALLRej")));//publicSite CookiesBar
+                    driver.FindElement(By.Id("cookGRALLRej")).Click();
+                }
+                catch
+                {
+                    try
+                    {
+                        wait.Until(ExpectedConditions.ElementIsVisible(By.Id("ccc-notify-accept!")));//wizz
+                        driver.FindElement(By.Id("ccc-notify-accept!")).Click();
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("checkbox_icon")));//developers
+                            driver.FindElement(By.ClassName("checkbox_icon")).Click();
+                            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("btn")));
+                            driver.FindElement(By.CssSelector("btn")).Click();
+                        }
+                        catch
+                        {
+                            try
+                            {
+                                wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='cookie - bar']/div/div[2]/div[3]/button")));//act4Greece
+                                driver.FindElement(By.XPath("//*[@id='cookie - bar']/div/div[2]/div[3]/button")).Click();
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+                    }
+
+                }
+                int number = driver.Manage().Cookies.AllCookies.Count;
+                List<Cookie> cooks = driver.Manage().Cookies.AllCookies.ToList();
+                oSheet.Cells[counter, 1] = line;
+                oSheet.Cells[counter, 2] = number;
+                foreach (Cookie cook in cooks)
+                {
+                    oSheet.Cells[counter, c2] = cook.Name;
+                    c2++;
+                }
+                counter++;
+                driver.Manage().Cookies.DeleteAllCookies();
+            }
+
+            infile.Close();
+
+            oXL.Visible = false;
+            oXL.UserControl = false;
+            oWB.SaveAs(@"C:\Users\e82337\Desktop\CookiesRej.xlsx", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            oWB.Close();
+            oXL.Quit();
+        }
+
+        [Test]
+        public void CookiesBarDefault()
+        {
+            String line;
+            System.IO.StreamReader infile = new System.IO.StreamReader(@"C:\Users\e82337\Desktop\domainUrl.txt");
+
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            Microsoft.Office.Interop.Excel._Worksheet oSheet;
+            oXL = new Microsoft.Office.Interop.Excel.Application();
+            oXL.Visible = true;
+            oWB = (Microsoft.Office.Interop.Excel._Workbook)(oXL.Workbooks.Add(""));
+            oSheet = (Microsoft.Office.Interop.Excel._Worksheet)oWB.ActiveSheet;
+
+            oSheet.Cells[1, 1] = "Domain";
+            oSheet.Cells[1, 2] = "Cookies";
+
+            int counter = 2;
+            while ((line = infile.ReadLine()) != null)
+            {
+                int c2 = 3;
+                driver.Navigate().GoToUrl(line);
+
+                int number = driver.Manage().Cookies.AllCookies.Count;
+                List<Cookie> cooks = driver.Manage().Cookies.AllCookies.ToList();
+                oSheet.Cells[counter, 1] = line;
+                oSheet.Cells[counter, 2] = number;
+                foreach (Cookie cook in cooks)
+                {
+                    oSheet.Cells[counter, c2] = cook.Name;
+                    c2++;
+                }
+                counter++;
+                driver.Manage().Cookies.DeleteAllCookies();
+            }
+
+            infile.Close();
+
+            oXL.Visible = false;
+            oXL.UserControl = false;
+            oWB.SaveAs(@"C:\Users\e82337\Desktop\CookiesDef.xlsx", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            oWB.Close();
+            oXL.Quit();
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
