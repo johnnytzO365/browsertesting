b4b08eef95d25f55678ee569273958b854bf16ad
Haris Giannopoulos <50793970+HarisMaster@users.noreply.github.com>
2019-12-16 12:00:49 +0200

    Update LanguageSelector.cs

--- SeleniumTutorial/LanguageSelector.cs
+++ SeleniumTutorial/LanguageSelector.cs
@@ -1,25 +1,51 @@
-﻿using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Support.UI;
-using SeleniumExtras.WaitHelpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Net;
+using System.Threading;
+using System.Configuration;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Support.UI;
+using System.Diagnostics;
+using System.ComponentModel;
+using OpenQA.Selenium.IE;
 
 namespace SeleniumTutorial
 {
     class LanguageSelector
     {
         IWebDriver driver;
+        WebDriverWait wait;
         [SetUp]
         public void StartBrowser()
         {
-            driver = new ChromeDriver(ConfigurationManager.AppSettings["ChromeDriverPath"]);
+            driver = new InternetExplorerDriver(ConfigurationManager.AppSettings["ChromeDriverPath"]);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(40));
+            driver.Navigate().GoToUrl((ConfigurationManager.AppSettings["ServerName"]));
+            Thread.Sleep(2000);
+
+            driver.FindElement(By.XPath("//*[@id='ctl00_PlaceHolderMain_ClaimsLogonSelector']")).Click();
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='ctl00_PlaceHolderMain_ClaimsLogonSelector']/option[2]")));
+            driver.FindElement(By.XPath("//*[@id='ctl00_PlaceHolderMain_ClaimsLogonSelector']/option[3]")).Click();
+            try
+            {
+                using (Process myProcess = new Process())
+                {
+                    myProcess.StartInfo.FileName = ConfigurationManager.AppSettings["ScriptPath"];
+                    myProcess.Start();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Thread.Sleep(4000);
         }
 
         [Test]
