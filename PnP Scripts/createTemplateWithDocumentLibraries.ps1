3a30a8c8dfd2ace09e64e88e2428cff5ed93398a
sindybousiou <sindyboussiou@gmail.com>
2020-01-08 16:05:35 +0200

    CookieChecker

--- PnP Scripts/createTemplateWithDocumentLibraries.ps1
+++ PnP Scripts/createTemplateWithDocumentLibraries.ps1
@@ -1,18 +1,18 @@
-﻿Import-Module SharePointPnPPowerShellOnline
+﻿Import-Module SharePointPnPPowerShell2013
 
 #connect
-$UserName = "e82331@nbg.gr"
-$PassWord = "Y?Ugjxgar"
+$UserName = "e82331"
+$PassWord = "p@ssw0rd"
 [SecureString]$SecurePassWord = ConvertTo-SecureString $PassWord -AsPlainText -Force
 $Credentials = New-Object System.Management.Automation.PSCredential($UserName,$SecurePassWord)
-$Url = "https://groupnbg.sharepoint.com/sites/Transformation"
+$Url = "http://v000080043:9993/sites/sp_team_nbg/"
 $connection = Connect-PnPOnline -Url $Url -Credentials $Credentials
 
 #create the template
-$templateUrl = "C:\Users\KyriakiBousiou\Desktop\TransformationTemplate\TransformationTemplate.xml"
+$templateUrl = "C:\Users\e82331\Desktop\TeamSiteTemplate\TeamSiteTemplate.xml"
 Get-PnPProvisioningTemplate -Out $templateUrl -Force -PersistBrandingFiles -PersistPublishingFiles -IncludeNativePublishingFiles -Handlers Navigation, Lists,PageContents, Pages, Files
 
-$targetPath = "C:\Users\KyriakiBousiou\Desktop\TransformationTemplate\"
+$targetPath = "C:\Users\e82331\Desktop\TeamSiteTemplate\"
 
 #get all document libraries
 $docLibs = Get-PNPList | Where-Object{$_.BaseTemplate -eq 101}
@@ -22,7 +22,7 @@ foreach($doc in $docLibs)
 {
     $docSplits = $null
     $docSplits = ($doc.DocumentTemplateUrl).Split("/")  #build the relative url to the document library
-    $docUrl = "/sites/Transformation/" + $docSplits[3]  #couldn't find a better way
+    $docUrl = "/sites/sp_team_nbg/" + $docSplits[3]  #couldn't find a better way
     ProcessFolder $docUrl ($targetPath+$docSplits[3])
 
     $tempfolders = Get-PnPProperty -ClientObject $doc.RootFolder -Property Folders
@@ -33,7 +33,7 @@ function ProcessSubFolders($folders, $targetPath) {
     foreach ($folder in $folders) {
         $tempurls = Get-PnPProperty -ClientObject $folder -Property ServerRelativeUrl 
         #Avoid Forms folders
-        if ($folder.Name -ne "Forms") #don't know why yet
+        if ($folder.Name -ne "Forms")
         {   
             $targetFolder = $targetPath +"\"+ $folder.Name;
             ProcessFolder $folder.ServerRelativeUrl.Substring($web.ServerRelativeUrl.Length) $targetFolder #check again first argument 
@@ -45,8 +45,6 @@ function ProcessSubFolders($folders, $targetPath) {
 
 function ProcessFolder($docUrl,$targetPath)
 {
-    Write-Output "This is what you need to know"
-    Write-Output $docUrl $targetPath
     $folder = Get-PnPFolder -RelativeUrl $docUrl
     $tempfiles = Get-PnPProperty -ClientObject $folder -Property Files
 
--- PnP Scripts/uploadToTeamSite.ps1
+++ PnP Scripts/uploadToTeamSite.ps1
@@ -1,5 +1,6 @@
 ﻿Import-Module SharePointPnPPowerShell2013
 
+#connect
 $UserName = "e82331"
 $pwd = "p@ssw0rd"
 [SecureString]$SecurePwd = ConvertTo-SecureString $pwd -AsPlainText -Force
@@ -7,25 +8,31 @@ $Credentials = New-Object System.Management.Automation.PSCredential($UserName,$S
 $Url = "http://v000080043:9993/sites/sp_team_nbg"
 Connect-PnPOnline -Url $Url -Credentials $Credentials
 
+#upload
 $localPath = "C:\Users\e82331\Downloads\CommunicationPages.xlsx"
 $name = Get-Date -Format "yyyy.MM.dd"
 $splib = "/CookieCheckerResults/"+$name
 Add-PnPFolder -Name $name -Folder "CookieCheckerResults"
 Add-PnPFile -Path $localPath -Folder $splib
+$message = "See CookieChecker Results in " + $Url+$splib
 
+#delete old files
 $folder = Get-PnPFolderItem -FolderSiteRelativeUrl "/CookieCheckerResults"
 if($folder.Count -ge 15)
 {
     if($folder[0].Name -ne "Forms")
     {
         Remove-PnPFolder -Name $folder[0].Name -Folder "/CookieCheckerResults" -Force
+        $message += "\n"+ $folder[0].Name+" removed!"
     }
     else
     {
         Remove-PnPFolder -Name $folder[1].Name -Folder "/CookieCheckerResults" -Force
+        $message += "\n"+ $folder[1].Name+" removed!"
     }
 }
 
+#send Email
 $UserName = "e82331@nbg.gr"
 $pwd = "p@ssw0rd"
 [SecureString]$SecurePwd = ConvertTo-SecureString $pwd -AsPlainText -Force
@@ -33,4 +40,4 @@ $Credentials = New-Object System.Management.Automation.PSCredential($UserName,$S
 $Url = "https://groupnbg.sharepoint.com/"
 Connect-PnPOnline -Url $Url -Credentials $Credentials
 
-Send-PnPMail -To e82331@nbg.gr,e82337@nbg.gr -Subject test -Body test
+Send-PnPMail -To e82331@nbg.gr,e82337@nbg.gr -Subject "CookieChecker Results" -Body $message
