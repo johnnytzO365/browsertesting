3bdd228d1ddf943575699ed90da02d2de255bf6e
sindybousiou <sindyboussiou@gmail.com>
2020-02-13 16:49:14 +0200

    QA Selenium

--- SeleniumTutorial/ContactFormQA.cs
+++ SeleniumTutorial/ContactFormQA.cs
@@ -17,7 +17,24 @@ namespace SeleniumTutorial
         [SetUp]
         public void StartBrowser()
         {
-            driver = new ChromeDriver(ConfigurationManager.AppSettings["ChromeDriverPath"]);
+            //setProperty("webdriver.chrome.driver", "C:\\Utility\\BrowserDrivers\\chromedriver.exe");
+            ChromeOptions options = new ChromeOptions();
+            //options.AddArguments("start-maximized");
+            //options.AddArguments("disable-infobars");
+            //options.AddArguments("--disable-extensions");
+            options.AddArguments("headless");
+            //options.AddArgument("--log-level=3");
+            //options.AddArgument("--no-sandbox");
+            //options.AddArgument("--silent");
+            //options.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36");
+
+            //DesiredCapabilities capability = new DesiredCapabilities();
+            //capability.SetCapability("Site Key", "6Len7l0UAAAAAJMgMNphPNacpB4SwLFAEf8urjBd");
+            //capability.SetCapability("Secret Key", "6Len7l0UAAAAAMdz-osYDxLCbGdyC91g7muIYSjN");
+            //options.AddAdditionalCapability("site Key", "6Len7l0UAAAAAJMgMNphPNacpB4SwLFAEf8urjBd");
+            //options.AddAdditionalCapability("secret key", "6Len7l0UAAAAAMdz-osYDxLCbGdyC91g7muIYSjN");
+            driver = new ChromeDriver(ConfigurationManager.AppSettings["ChromeDriverPath"],options);
+            //driver.Get("https://rsps100.com/vote/760");
             driver.Manage().Window.Maximize();  //to use the desired width of window
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
         }
@@ -89,6 +106,20 @@ namespace SeleniumTutorial
             {
                 Assert.Fail("Couldn't find contact by email radio button!");
             }
+            /*try
+            {
+                string tempSelector = "captchainput";
+                IWebElement temp = driver.FindElement(By.Id(tempSelector));
+
+                string iframeSelector = "./div/div/iframe";
+                IWebElement iframe = temp.FindElement(By.XPath(iframeSelector));
+
+                driver.SwitchTo().Frame(iframe);
+            }
+            catch
+            {
+                Assert.Fail("Couldn't switch to iFrame for Recaptcha!");
+            }
             try
             {
                 string recaptchaSelector = "recaptcha-checkbox-border";
@@ -99,16 +130,17 @@ namespace SeleniumTutorial
             catch
             {
                 Assert.Fail("Couldn't find recaptcha checkbox!");
-            }
+            }*/
             try
             {
+                driver.SwitchTo().ParentFrame();
                 string submitSelector = "[id$='btnSubmit']";
                 IWebElement submit = driver.FindElement(By.CssSelector(submitSelector));
                 submit.Click();
+                Thread.Sleep(3000);
             }
             catch
             {
-                Assert.Fail("Couldn't find Submit Button!");
             }
             /*
             driver.Navigate().GoToUrl((ConfigurationManager.AppSettings["ServerName"]) + "Lists/ContactForms/AllItems.aspx");  //go to contact list
@@ -412,6 +444,12 @@ namespace SeleniumTutorial
             wait.Until(ExpectedConditions.AlertIsPresent());
             driver.SwitchTo().Alert().Accept();
             Thread.Sleep(1000);
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Quit();
         }
     }
 }
