b85de24e8681dd5ed6efeca1863fdab83f8b946d
Cindy Bousiou <sindyboussiou@gmail.com>
2020-06-23 16:58:15 +0300

    send email

--- NBG.MyNBGPortal/NBG.MyNBGPortal/Email_Alerting/Email_Alerting.cs
+++ NBG.MyNBGPortal/NBG.MyNBGPortal/Email_Alerting/Email_Alerting.cs
@@ -4,6 +4,7 @@ using Microsoft.SharePoint;
 using Microsoft.SharePoint.Utilities;
 using Microsoft.SharePoint.Workflow;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 
 namespace NBG.MyNBGPortal.Email_Alerting
 {
@@ -42,6 +43,35 @@ namespace NBG.MyNBGPortal.Email_Alerting
                 {
                     throw ex;
                 }
+
+                SPList groups = web.GetList("/greek/test/Lists/Expand Groups");
+                SPListItemCollection items = groups.GetItems();
+                foreach (SPListItem item in items)
+                {
+                    string name = item["User"].ToString();
+                    name = name.Substring(3, name.Length - 3);
+                    string email = web.EnsureUser(name).Email;
+
+                    StringDictionary headers = new StringDictionary();
+                    headers.Add("from", "sender@domain.com");
+                    headers.Add("to", email);
+                    headers.Add("subject", properties.ListItem["E-mail Subject"].ToString());
+                    headers.Add("fAppendHtmlTag", "True"); //To enable HTML format
+
+                    System.Text.StringBuilder strMessage = new System.Text.StringBuilder();
+                    strMessage.Append(properties.ListItem["E-mail Body"].ToString());
+                    SPUtility.SendEmail(web, headers, strMessage.ToString());
+                    /*
+                    string filePath = "http://vm-sp2013/greek/test/Shared%20Documents/log.txt";
+                    using (Stream file = System.IO.File.OpenWrite(filePath))
+                    {
+                        string message = "Email sent to " + name;
+                        byte[] buffer = Encoding.ASCII.GetBytes(message);
+                        //Encoding.GetBytes(message, 0, message.Length, buffer, 0); 
+                        file.Write(buffer, 0, message.Length);
+                    }*/
+                }
+            
             }
 
             
