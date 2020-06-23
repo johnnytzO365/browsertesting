e87b78092006e47265aa740a5d8cf44a7e4ebb63
Haris Giannopoulos <50793970+HarisMaster@users.noreply.github.com>
2020-06-23 14:10:27 +0300

    TextMail

--- NBG.MyNBGPortal/NBG.MyNBGPortal/Email_Alerting/Email_Alerting.cs
+++ NBG.MyNBGPortal/NBG.MyNBGPortal/Email_Alerting/Email_Alerting.cs
@@ -3,6 +3,7 @@ using System.Security.Permissions;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.Utilities;
 using Microsoft.SharePoint.Workflow;
+using System.Collections.Generic;
 
 namespace NBG.MyNBGPortal.Email_Alerting
 {
@@ -17,6 +18,33 @@ namespace NBG.MyNBGPortal.Email_Alerting
         public override void ItemUpdated(SPItemEventProperties properties)
         {
             base.ItemUpdated(properties);
+            using (SPWeb web = properties.OpenWeb())
+            {
+                try
+                {
+                    
+                    SPListItem currentItem = properties.ListItem;
+                    string EmailBody = currentItem["Anakoinoseis"].ToString();
+                    var body1 = EmailBody.Split(new[] {";"}, StringSplitOptions.None);
+                    var email = "";
+                    currentItem["E_x002d_mail_x0020_Body"] = "";
+                    for (int i = body1.Length; i > 1; i-=2 )
+                    {
+                        email = currentItem["E_x002d_mail_x0020_Body"].ToString();
+                        var body = body1[i - 1].Split(new[] { "#" }, StringSplitOptions.None);
+                        currentItem["E_x002d_mail_x0020_Body"] = email + body[1] + "<br>";
+                    }
+                    
+                    currentItem.Update();
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            
         }
 
 
